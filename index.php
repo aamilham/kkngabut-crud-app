55ed6b45e8cd351b7495efc699dd6657aa79a32f
aamilham <aamilham13@gmail.com>
2024-07-24 13:41:05 +0700

    add some bootstrap

--- index.php
+++ index.php
@@ -4,43 +4,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KKN gabut aam</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+    <div class="mx-5 mt-3">
     <h2>Form Input Absensi Warga Desa</h2>
     <form method="POST" action="insert.php">
-        <label for="id">ID:</label>
-        <input type="text" name="id" required><br>
-        <label for="nama">Nama:</label>
-        <input type="text" name="nama" required><br>
-        <label for="dusun">Dusun:</label>
-        <input type="text" name="dusun" required><br>
-        <input type="submit" value="Submit">
+        <div class="">
+            <label for="id" class="form-label">ID</label>
+            <input type="text" class="form-control" name="id" required><br>
+        </div>
+        <div class="">
+            <label for="nama" class="form-label">Nama</label>
+            <input type="text" class="form-control" name="nama" required><br>
+        </div>
+        <div class="">
+            <label for="dusun" class="form-label">Dusun</label>
+            <input type="text" class="form-control" name="dusun" required><br>
+        </div>
+        <input type="submit" value="Submit" class="btn btn-primary mb-4">
     </form>
 
-    <h2>Data Warga</h2>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Nama</th>
-            <th>Dusun</th>
-            <th>Action</th>
-        </tr>
-        <?php
-        /* link php into database config */
-        include 'config.php';
-        $result = mysqli_query($conn, "SELECT * FROM `warga_desa`");
+    <h2>Data Warga</h2> 
+    <table class="table table-striped" border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Dusun</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            /* link php into database config */
+            include 'config.php';
+            $result = mysqli_query($conn, "SELECT * FROM `warga_desa`");
 
-        /* fetching data from database */
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            echo "<td>".$row['id']."</td>";
-            echo "<td>".$row['nama']."</td>";
-            echo "<td>".$row['dusun']."</td>";
-            echo "<td><a href='delete.php?id=".$row['id']."'>Delete</a></td>";
-            echo "</tr>";
-        }
+            /* fetching data from database */
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>".$row['id']."</td>";
+                echo "<td>".$row['nama']."</td>";
+                echo "<td>".$row['dusun']."</td>";
+                echo "<td><a href='delete.php?id=".$row['id']."'>Delete</a></td>";
+                echo "</tr>";
+            }
 
-        ?>
+            ?>
+        </tbody>
     </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
